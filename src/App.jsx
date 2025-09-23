3c96c2007849eb56355bb1e8c313f5d47102190f
YengHer919 <heryeng58@gmail.com>
2025-09-23 13:30:23 -0700

    better styling + search bar

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,26 @@
 import "./styles.css";
 import "leaflet/dist/leaflet.css";
 import { MapContainer, TileLayer, Marker, Popup } from "react-leaflet";
-
 import { Icon, divIcon, point } from "leaflet";
 
 export default function App() {
   return (
-    <MapContainer center={[48.8566, 2.3522]} zoom={13}>
-      <TileLayer
-        attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-        url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
-      />
-    </MapContainer>
+    <>
+      <header className="topbar">
+        <div className="header-left">
+          <h1>Map Search</h1>
+        </div>
+        <div className="header-center">
+          <input type="text" placeholder="Search restaurants..." />
+        </div>
+      </header>
+
+      <MapContainer center={[37.3382, -121.8863]} zoom={13}>
+        <TileLayer
+          attribution='&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+          url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png"
+        />
+      </MapContainer>
+    </>
   );
 }
