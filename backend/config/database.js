706731933690827efdfd707de268ffcb8a8c6d0f
Muyi Lin <muyi.lin@gmail.com>
2025-11-23 14:24:24 -0800

    apply local WIP after rebase

--- backend/config/database.js
+++ backend/config/database.js
@@ -4,7 +4,7 @@ import mysql from 'mysql2/promise';
 const pool = mysql.createPool({
   host: 'localhost',
   user: 'root',
-  password: '*', // UPDATE THIS WITH YOUR MYSQL PASSWORD
+  password: 'saeknsaekn123$', // UPDATE THIS WITH YOUR MYSQL PASSWORD
   database: 'too_good_to_go',
   waitForConnections: true,
   connectionLimit: 10, // Maximum of 10 concurrent database connections
